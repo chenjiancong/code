23734dcde498a883f255d8b128179aa19e69ac1d
jack <jack@localhost.localdomain>
2015-02-09 19:07:26 +0800

    aa

---
+++ regedit.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+#Filename: regedit.py
+#-*- coding:utf-8 -*-
+
+name = raw_input("Enter a name: ")
+age = int(raw_input("Enter age: "))
+def enroll(name,age):
+    print '%s'%name
+    print '%d'%age
+
+enroll(name,age)
