16eb6e9e9b192d7e62abc41d8be816e47246af8c
jack <jack@localhost.localdomain>
2015-02-14 23:19:47 +0800

    regedit

--- regedit.py
+++ regedit.py
@@ -2,10 +2,12 @@
 #Filename: regedit.py
 #-*- coding:utf-8 -*-
 
-name = raw_input("Enter a name: ")
-age = int(raw_input("Enter age: "))
-def enroll(name,age):
+#name = raw_input("Enter a name: ")
+#age = int(raw_input("Enter age: "))
+#city = raw_input("Enter a city name: ")
+def enroll(name,age,city='Beijin'):
     print '%s'%name
     print '%d'%age
+    print '%s'%city
 
-enroll(name,age)
+enroll('jj',22)
