98bde4142071fa2ff5f8ae8d6d5a1ed3f21ce0d2
jack <jack@localhost.localdomain>
2015-02-14 22:16:35 +0800

    py

--- str.py
+++ str.py
@@ -2,10 +2,6 @@
 #-*- coding: utf-8 -*-
 #Filename: str.py
 
-#ord('字母'),将字母转换成ASCII码
-str = raw_input('输入一个要查ASCII的字母:')
-print '%s ==>> ASCII is: '%str,ord(str)
+ord(raw_input('Enter one str'))
+#char(int(raw_input('Enter one number')))
 
-#chr(数值),将数值转换成对应的字母
-str2 = int(raw_input('输入一个要转换成字母值:'))
-print '%d ==>> 对应字母:'%str2,chr(str2)
