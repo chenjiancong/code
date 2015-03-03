69b2f6d9f6f93c36256f529e155b37651003ce34
chenjiancong <chenjiancong@gmail.com>
2015-03-03 22:19:18 +0800

    a

--- get_img.py
+++ get_img.py
@@ -13,18 +13,11 @@ def get_html(url):
 #print get_html("http://tieba.baidu.com/p/3553794829")
 
 def get_img(html):
-    reg = r'src="(.+?\.png)"'
+    reg = 'src="(.+?\.jpg)"'
     imgre = re.compile(reg)
     imglist = re.findall(imgre,html)
     return imglist
 
-#    x = 0
-#    for imgurl in imglist:
-#        urllib.urlretrieve(imgurl,'%s.png'%x)
-#        x = x+1
-
-html = get_html("http://www.cnblogs.com/huxi/archive/2010/07/04/1771073.html")
-get_img(html)
-
-
+html = get_html("http://bulaoge.net/")
+print get_img(html)
 
--- getimg.py
+++ getimg.py
@@ -5,29 +5,18 @@
 import re
 import urllib
 
-def get_url(url):
+def gethtml(url):
     page = urllib.urlopen(url)
     html = page.read()
     return html
 
-#print get_url('http://www.cnblogs.com/huxi/archive/2010/07/04/1771073.html')
+#print gethtml('http://bohaishibei.com/post/7883/')
 
-def get_img(html):
-    regex = r'src="(.+?\.png)"'
+def getimg(html):
+    regex = r'<img.*? src="(.+?.jpg)"title.+?>'
     patten = re.compile(regex)
-    img_url = re.findall(patten,html)
-#    return img_url
-    x = 1
-    for img in img_url:
-        urllib.urlretrieve(img,'%s.jpg'%x)
-        x +=1
+    url = re.findall(patten,html)
+    return url
 
-html = get_url('http://www.cnblogs.com/huxi/archive/2010/07/04/1771073.html')
-get_img(html)
-
-
-
-
-
-
-
+html = gethtml('http://bohaishibei.com/post/7883/')
+print getimg(html)
