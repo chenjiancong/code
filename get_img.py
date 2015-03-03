152601dee2deba0c6a106ae4df5634331e521c0e
chenjiancong <chenjiancong@gmail.com>
2015-03-03 09:03:52 +0800

    a

--- get_img.py
+++ get_img.py
@@ -13,11 +13,12 @@ def get_html(url):
 #print get_html("http://tieba.baidu.com/p/3553794829")
 
 def get_img(html):
-    reg = 'src="(.+?\.jpg)"'
+   # reg = r'pic_ext="(.+?)"'
+    reg = r'src="(.+?\.png)"'
     imgre = re.compile(reg)
     imglist = re.findall(imgre,html)
     return imglist
 
-html = get_html("http://tieba.baidu.com/p/3553794829")
+html = get_html("http://www.cnblogs.com/huxi/archive/2010/07/04/1771073.html")
 print get_img(html)
 
