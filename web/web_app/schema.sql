0355f502b35625d8ef11aed1c93b9855e13dc1cf
chenjiancong <chenjiancong@gmail.com>
2015-04-23 17:54:55 +0800

    0423work

--- web/web_app/schema.sql
+++ web/web_app/schema.sql
@@ -8,39 +8,39 @@ use webdb;
 grant select, insert, update, delete on awesome.* to 'www-data'@'localhost' identified by 'www-data';
 
 create table users (
-id varchar(50) not null,
+id int(4) not null default '1000',
 email varchar(50) not null,
-password varchar(50) not null,
-admin bool not null,
+password int(50) not null,
+admin int(1) not null default '0',
 name varchar(50) not null,
 image varchar(500) not null,
-created_at real not null,
+created_at float(24) not null,
 unique key idx_email (email),
 key idx_created_at (created_at),
 primary key (id)
 )engine=innodb default charset=utf8;
 
 create table blogs (
-id varchar(50) not null,
-user_id varchar(50) not null,
+id int(20) not null,
+user_id int(50) not null,
 user_name varchar(50) not null,
 user_imge varchar(500) not null,
 name varchar(50) not null,
 summary varchar(50) not null,
 content varchar(200) not null,
-created_at real not null,
+created_at float(24) not null,
 key idx_created_at (created_at),
 primary key (id)
 )engine=innodb default charset=utf8;
 
 create table comments (
-id varchar(50) not null,
-blog_id varchar(50) not null,
-user_id varchar(50) not null,
+id int(20) not null,
+blog_id int(50) not null,
+user_id int(50) not null,
 user_name varchar(50) not null,
 user_imge varchar(500) not null,
 content mediumtext not null,
-created_at real not null,
+created_at float(24) not null,
 key idx_created_at (created_at),
 primary key (id)
 )engine=innodb default charset=utf8
