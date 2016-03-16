f3910aa014e15c9564692583a1736d82bc639afd
chenjiancong <chenjiancong@gmail.com>
2016-03-16 23:17:37 +0800

    app/login

---
+++ app/day-06/hello.py
@@ -0,0 +1,50 @@
+from flask import Flask, render_template, request, session, redirect, flash, url_for
+from flask.ext.bootstrap import Bootstrap
+
+#  Configuration
+USERNAME = 'admin'
+PASSWORD = '123'
+SECRET_KEY = 'development key'
+DEBUG = True
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+app.config.from_envvar('FLASK_SETTINGS', silent=True)
+bootstrap = Bootstrap(app)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_entry():
+    pass
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != app.config['USERNAME']:
+            error = 'Invalid username'
+        elif request.form['password'] != app.config['PASSWORD']:
+            error = 'Invalid password'
+        else:
+            session['logged_in'] = True
+            flash('You were logged in')
+            return redirect(url_for('index'))
+    return render_template('login.html', error=error)
+
+@app.route('/user/<name>')
+def user(name):
+    return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0')
