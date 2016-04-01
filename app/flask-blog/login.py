159b4de69df469da5ce796cb58d93e724ab2e1f6
chenjiancong <chenjiancong@gmail.com>
2016-04-01 23:39:43 +0800

    0401

--- app/flask-blog/login.py
+++ app/flask-blog/login.py
@@ -1,13 +1,9 @@
-from flask import Flask, render_template, flash, redirect, url_for, flash
+from flask import Flask, render_template, flash, redirect, url_for, flash, session
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.wtf import Form
-from wtforms import StringField,PasswordField, SubmitField
+from wtforms import StringField,PasswordField, SubmitField, TextAreaField
 from wtforms.validators import Required
-from flask.ext.login import login_user
-
-USERNAME = 'admin'
-PASSWORD = '123'
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
@@ -22,32 +18,64 @@ class LoginForm(Form):
     password = PasswordField('Password', validators = [Required()])
     submit = SubmitField('Login', validators = [Required()])
 
+class EditForm(Form):
+    email = StringField('Email', validators = [Required()])
+    username = StringField('Username', validators = [Required()])
+    about_me = TextAreaField('About me', validators = [Required()])
+    submit = SubmitField('Submit')
+
 class Users(db.Model):
     __tablename__ = 'Users'
     uid = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(50))
     username = db.Column(db.String(50))
     password = db.Column(db.String(50))
+    about_me = db.Column(db.String(50))
+    member_since = db.Column(db.DateTime())
 
 @app.route('/auth/login', methods = ['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        #  if form.username.data != app.config['USERNAME']:
-            #  flash('wrong')
-        #  elif form.password.data != app.config['PASSWORD']:
-            #  flash('wrong')
-        #  else:
-            #  flash('logged')
-            #  return 'hello'
         user = Users.query.filter_by(username=form.username.data).first()
         if form.username.data != user.username:
-            flash('username wrong')
+            flash(' wrong username')
+            error = 'Invalid username'
         elif form.password.data != user.password:
-            flash('password wrong')
+            flash(' wrong password')
+            error = 'Invalid password'
         else:
-            return 'hello'
-    return render_template('login3.html', form = form)
+            session['logged_in'] = True
+            flash('You were Logged in ')
+            return redirect(url_for('index'))
+            #  return redirect(url_for('index'))
+    return render_template('login.html', form = form)
+
+@app.route('/auth/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect(url_for('login'))
+
+@app.route('/user/<username>', methods = ['GET', 'POST'])
+def user(username):
+    form = EditForm()
+    if form.validate_on_submit():
+        user = Users.query.filter_by(username=form.username.data).first()
+        user.email = form.email.data
+        user.username = form.username.data
+        user.about_me = form.about_me.data
+        db.session.add(user)
+        return 'hello'
+    form.email = Users.email
+    form.username = Users.username
+    form.password = Users.password
+    form.about_me = Users.about_me
+    return render_template('edit_profile.html', form = form)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug = True)
--- app/flask-blog/register.py
+++ app/flask-blog/register.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, flash
 from flask.ext.bootstrap import Bootstrap
 from flask.ext.wtf import Form
-from wtforms import  StringField, PasswordField, SubmitField, ValidationError
+from wtforms import  StringField, PasswordField, SubmitField,TextAreaField,  ValidationError
 from wtforms.validators import Required, EqualTo, DataRequired
 from flask.ext.sqlalchemy import SQLAlchemy
 
@@ -13,10 +13,11 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:cjcdb@localhost/flaskr'
 
 class User(db.Model):
     __tablename__ = 'Users'
-    id = db.Column(db.Integer, primary_key = True)
+    uid = db.Column(db.Integer, primary_key = True)
     email = db.Column(db.String(50))
     username = db.Column(db.String(50))
     password = db.Column(db.String(50))
+    about_me = db.Column(db.Text())
 
 class Register(Form):
     email = StringField('Email', validators = [DataRequired()])
@@ -24,6 +25,7 @@ class Register(Form):
     password = PasswordField('Password', validators = [DataRequired()])
     password2 = PasswordField('Comfirm password', validators =
                             [DataRequired(), EqualTo('password', message = 'Password not match')])
+    about_me = TextAreaField('About me', validators = [DataRequired()])
     submit = SubmitField('Register', validators = [DataRequired()])
 
 @app.route('/auth/register', methods = ['GET', 'POST'])
@@ -32,7 +34,8 @@ def register():
     if form.validate_on_submit():
         user = User(email = form.email.data,
                     username = form.username.data,
-                    password = form.password.data)
+                    password = form.password.data,
+                    about_me = form.about_me.data)
         db.session.add(user)
         db.session.commit()
         flash('You can login')
