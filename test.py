4af9f75977f7661af62df4b1509cef2827d9b953
chenjiancong <chenjiancong@gmail.com>
2017-07-21 03:43:59 +0800

    720

--- test.py
+++ test.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-
 def calc(*numbers):
-    '''    hello    '''
     sum = 0
-    for i in numbers:
-        sum = i + i
+    for n in numbers:
+        sum = sum + n * n
     return sum
 
 print(calc(1,2))
-print(calc.__doc__)
