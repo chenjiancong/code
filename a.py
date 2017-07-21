d72b48d1fe61ea6d75f6a9ff24dc5fc4c70bc45b
chenjiancong <chenjiancong@gmail.com>
2017-07-22 03:17:58 +0800

    a.py

--- a.py
+++ a.py
@@ -6,8 +6,9 @@ def calc(*numbers):
     '''    hello    '''
     sum = 0
     for i in numbers:
-        sum = i + i
+        sum = sum + i
     return sum
 
-print(calc(1,2))
+
+print(calc(1,2,4))
 print(calc.__doc__)
