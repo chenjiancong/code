503ae1338599d99cb22e973857b1075c6c27d07a
chenjiancong <chenjiancong@gmail.com>
2017-08-07 18:49:07 +0800

    a.py and test.py

--- a.py
+++ a.py
@@ -1,12 +1,27 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-class Foo:
-    def Bar(self):
-        print('Bar')
-    def Hello(self, name):
-        print('i am %s' %name)
 
-obj = Foo()
-obj.Bar()
-obj.Hello('Jack')
+class Foo:
+    def __init__(self, name, age):
+        self.name = name
+        self.age  = age
+
+    # 间接调用
+    def detail(self):
+        print(self.name)
+        print(self.age)
+
+
+# 直接调用
+#obj1 = Foo('Tom', 18)
+#print(obj1.name)
+#print(obj1.age)
+#
+#obj2 = Foo('Jack', 20)
+#print(obj2.name)
+#print(obj2.age)
+
+# 间接调用
+obj1 = Foo('Tom', 18)
+obj1.detail()
---
+++ test.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+# 封装
+class Foo:
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age  = age
+        self.gender  = gender
+
+    def kanchai(self):
+        print('{0},{1},{2}, 上山去砍柴' .format(self.name,self.age,self.gender))
+
+xiaoming = Foo('小明', 10, '男')
+xiaoming.kanchai()
+
+# 继承
+class Animal:
+    def eat(self):
+        print('%s 吃'%self.name)
+    def drint(self):
+        print('%s 喝'%self.name)
+
+class Cat(Animal):
+    def __init__(self, name):
+        self.name = name
+
+    def cry(self):
+        print('喵喵叫')
+
+c1 = Cat('小花猫')
+c1.eat()
+c1.cry()
