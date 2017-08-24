0144445b9abc2473962fd86e7d3de957f726908f
chenjiancong <chenjiancong@gmail.com>
2017-08-24 23:15:22 +0800

    0824home

--- test_decorator.py
+++ test_decorator.py
@@ -1,15 +1,30 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-def log(func):
-    def wrapper(*args, **kw):
+def func1(n1):
+    def func2(n2):
+        print(n2)
+        return n1 + n2
+    return func2
+
+a = func1(20)
+print(a(100))
+
+def func2(func):
+    def worker(*args, **kw):
         print('call {}'.format(func.__name__))
         return func(*args, **kw)
-    return wrapper
+    return worker
+@func2
+def Hello():
+    print('Hello')
 
-@log
-def now():
-    print('2017-08-12')
+a = Hello
+print(a())
 
-f = now
-f()
+d={'tom':90, 'peter':80}
+for i in d.values():
+    print(i)
+
+for k, v in d.items():
+    print(k, v)
