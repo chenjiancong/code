ac78bad9323ba96b3023feeed6388ccf3568de79
chenjiancong <chenjiancong@gmail.com>
2017-09-01 23:05:50 +0800

    0901home

--- test1.py
+++ test1.py
@@ -1,33 +1,22 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-from functools import reduce
+# break
 
-# 阶乘1
-def func1(n):
-    return reduce(lambda x, y: x * y, range(1, n + 1))
-print(func1(4))
+#while True:
+#    s = input('Enter something:')
+#    if s == 'quit':
+#        break
+#    else:
+#        print('len(s) is: ', len(s))
+#print('Done')
 
-def func11(n):
-    if n == 1:
-        return 1
-    return n * func11(n - 1)
-print(func11(4))
-
-
-# 求和1
-def func2(n):
-    sum = 0
-    for i in range(n + 1):
-        sum += i
-    return sum
-print(func2(100))
-
-def func3(n):
-    sum = 0
-    i = 1
-    while n - i >= 0:
-        sum += i
-        i += 1
-    return sum
-print(func3(100))
+while True:
+    s = input('Enter something:')
+    if s == 'quit':
+        break
+    elif len(s) < 3:
+        continue
+    else:
+        print('len(s) is: ', len(s))
+print('Done')
