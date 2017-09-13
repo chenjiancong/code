935c8489a4a8730c836588c27b86ce4c16b07c09
chenjiancong <chenjiancong@gmail.com>
2017-09-13 09:43:01 +0800

    0913work

---
+++ py_game/a.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+
+i = int(input('enter something'))
+def func(i):
+    res = i * 2
+    print(res)
+func(i)
--- py_game/g_00.py
+++ py_game/g_00.py
@@ -1,19 +1,12 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-# 1,2,3,4 组合成无重复3位数
-#for x in range(100, 500, 100):
-#    for y in range(10, 50, 10):
-#        for z in range(1, 5):
-#            print(filter(x + y + z))
-
-def func(n):
-    n <= 10
-g = [a * 1 for a in range(1, 444)]
-a = filter(func, g)
-print(list(a))
-#x = [a * 1 for a in range(1, 5)]
-#y = [b * 10 for b in range(1, 5)]
-#z = [c * 100 for c in range(1, 5)]
-#print(x, y, z)
-
+# 1,2,3,4 能组成多少个无重复3位数? 分别是哪些？
+res = []
+for x in range(1, 5):
+    for y in range(1, 5):
+        for z in range(1, 5):
+            if x !=y and y != z and x != z:
+                res.append(x * 100 + y * 10 + z)
+print(len(res))
+print(res)
