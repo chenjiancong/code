46efdcb1e16de5780cde02fdbd0f107a5f912270
chenjiancong <chenjiancong@gmail.com>
2017-09-16 19:14:08 +0800

    0916work

---
+++ a_edit.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+# encoding: utf-8
+
+from sys import argv
+
+script, filename = argv
+
+print('We\'re going to erase {}.'.format(filename))
+print('If you don\'t want that, hit CTRL-C (^C)')
+print('If you do want that, hit RETURN.')
+
+input('?')
+
+print('Opening the file...')
+target = open(filename, 'a') # a 如果文件存在则在结尾追加；不存在就创建
+
+#print('Truncating the file. Goodbye!')
+#target.truncate()
+
+print('Now I\'m going to ask you for three lines.')
+
+line1 = input('line 1: ')
+line2 = input('line 2: ')
+line3 = input('line 3: ')
+
+print('I\'m going to write these to the file.')
+
+target.write(line1)
+target.write('\n')
+target.write(line2)
+target.write('\n')
+target.write(line3)
+target.write('\n')
+
+print('And finally, we close it.')
+target.close()
