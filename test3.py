e84706eadd15d65d1b000cbfc412b97418fc5ccb
chenjiancong <chenjiancong@gmail.com>
2017-10-09 08:49:01 +0800

    1009work

--- test3.py
+++ test3.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-for i in range(1, 5):
-    for j in range(1, 5):
-        for k in range(1, 5):
-            if i !=j and i !=k and j !=k:
-                print(i, j, k)
+def sort_words(words):
+    return sorted(words)
